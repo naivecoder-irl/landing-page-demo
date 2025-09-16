8f1dea99c707924a208f576091f375a1bdd8e2e0
Steve Chen <dexiang.chen1@outlook.com>
2025-09-16 17:51:14 +0100

    mod: move Insights component back to main landing page

--- landing-page-frontend/src/app/page.tsx
+++ landing-page-frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "@/components/hero";
 import Insights from "@/components/insights";
+import InsightCards from "@/components/artricle-card";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
@@ -12,7 +13,8 @@ export default function Home() {
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
         <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
           <Hero />
-          <Insights />
+          {/* <Insights /> */}
+          <InsightCards />
           <Footer />
         </main>
       </div>
